13b462ef880d5806d8882207089f463c3efa6462
QueenDomzy <qdomzy@gmail.com>
2025-08-30 02:01:50 +0100

    git push

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "SwiftStay frontend init"
 git branch -M main
-git remote add origin <your-repo-url>
+git remote add origin <https://github.com/QueenDomzy/SwiftStay-main>
 git push -u origin main
