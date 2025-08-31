a5aa4e9119d80575c1718b407a3864747455a66e
QueenDomzy <qdomzy@gmail.com>
2025-08-31 21:21:22 +0100

    git push

--- Backend/src/chat/chat.dto.ts
+++ Backend/src/chat/chat.dto.ts
@@ -1,6 +1,11 @@
 // src/chat/chat.dto.ts
 import { IsArray, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
+import OpenAI from "openai";
+
+cost messages: OpenAI.Chat.ChatCompletionMessageParam[] =  [
+  { role: "user", coontent: "Hello" }
+  ];
 
 export class ChatMessageDto {
   role!: "user" | "assistant" | "system";
