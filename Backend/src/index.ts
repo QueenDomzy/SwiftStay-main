bc8bdbe84841ef15c19b80b53c466911fa2b88ce
QueenDomzy <qdomzy@gmail.com>
2025-08-31 10:46:53 +0100

    git push

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,12 +3,17 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth";
 import reservationRoutes from "./routes/reservations";
+import paystackRoutes from "./routes/paystack.routes";
+import flitterwaveRoutes from "./routes/flutterwave.routes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Routes
+app.use("/paystack", paystackRoutes);
+app.use("/flutterwave", flutterwaveRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/reservations", reservationRoutes);
 
