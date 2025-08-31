2643a7e20f1cc6038bd2ba3755ae890d18c0808a
QueenDomzy <qdomzy@gmail.com>
2025-08-31 18:49:50 +0100

    git push

--- Backend/src/routes/flutterwave.routes.ts
+++ Backend/src/routes/flutterwave.routes.ts
@@ -8,6 +8,17 @@ router.post("/initialize", async (req: Request, res: Response) => {
   try {
     const { email, amount, txRef, redirectUrl } = req.body;
     const payment = await flutterwaveService.initializePayment(email, amount, txRef, redirectUrl);
+    flw.Charge.card({
+  card_number: payload.card_number,
+  cvv: payload.cvv,
+  expiry_month: payload.expiry_month,
+  expiry_year: payload.expiry_year,
+  currency: "NGN",
+  amount: payload.amount,
+  redirect_url: "https://swiftstay-main.onrender.com/payment/callback",
+  email: payload.email,
+  fullname: payload.fullname,
+});
     res.json(payment);
   } catch (err: any) {
     res.status(500).json({ error: err.message });
