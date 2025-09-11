4bebad959c2fd9251269041d7fca5c36c3729839
QueenDomzy <qdomzy@gmail.com>
2025-09-11 23:59:44 +0100

    git push

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,7 +1,3 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
-  });
-
-  export default api;
+export const fetcher = (url: string) => axios.get(url).then(res => res.data);
