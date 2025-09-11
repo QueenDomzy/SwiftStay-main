f3799c9d3d51002641749307cef0156b19df01e2
QueenDomzy <qdomzy@gmail.com>
2025-09-11 22:23:08 +0100

    git push

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -3,8 +3,13 @@ import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class AppController {
+  @Get()
+  getRoot() {
+    return { message: "🚀 SwiftStay Backend is running!" };
+  }
+
   @Get("/health")
   health() {
     return { status: "ok" };
   }
-  }
+}
