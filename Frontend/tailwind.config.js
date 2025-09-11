863b436d310e24ca962292291f38cfc5037214e1
QueenDomzy <qdomzy@gmail.com>
2025-09-11 23:49:17 +0100

    git push

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,21 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        swiftBlack: "#000000",
-        swiftWhite: "#FFFFFF",
-        swiftGold: "#FFD700",
-        swiftGreen: "#006400",
-        swiftBlue: "#1E90FF"
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-        serif: ["Merriweather", "serif"]
+        gold: "#FFD700"
       }
     }
   },
