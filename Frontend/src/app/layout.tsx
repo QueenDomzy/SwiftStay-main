f0a48c12047d7500a1199d10f019a80cf2d25151
QueenDomzy <qdomzy@gmail.com>
2025-09-12 00:15:15 +0100

    git add src/styles/globals.css git commit -m "Add globals.css" git push

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,15 +1,2 @@
-import './styles/globals.css';
-import { Navbar } from '../components/Navbar';
-import { Footer } from '../components/Footer';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-      <html lang="en">
-            <body className="min-h-screen flex flex-col">
-                    <Navbar />
-                            <main className="flex-1">{children}</main>
-                                    <Footer />
-                                          </body>
-                                              </html>
-                                                );
-                                                }
+// src/app/layout.tsx
+import '../styles/globals.css';
