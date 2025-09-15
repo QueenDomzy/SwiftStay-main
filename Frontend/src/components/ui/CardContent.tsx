950df1bd884649ffd9579d1f1359c210d6de7e4e
QueenDomzy <qdomzy@gmail.com>
2025-09-15 02:56:36 +0100

    npm run build

---
+++ Frontend/src/components/ui/CardContent.tsx
@@ -0,0 +1,13 @@
+import { ReactNode, HTMLAttributes } from "react";
+
+interface CardContentProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+
+export function CardContent({ children, className, ...props }: CardContentProps) {
+  return (
+    <div className={`p-4 ${className || ""}`} {...props}>
+      {children}
+    </div>
+  );
+}
