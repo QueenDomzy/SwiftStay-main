ce1772986954278a0bc01175f8a593f65ac6029b
QueenDomzy <qdomzy@gmail.com>
2025-09-15 17:12:16 +0100

    Git push

---
+++ Backend/src/hotels/hotels.module.ts
@@ -0,0 +1,14 @@
+// src/hotels/hotels.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hotel } from './hotel.entity';
+import { HotelsService } from './hotels.service';
+import { HotelsController } from './hotels.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Hotel])],
+  providers: [HotelsService],
+  controllers: [HotelsController],
+  exports: [HotelsService],
+})
+export class HotelsModule {}
