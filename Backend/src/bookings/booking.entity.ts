6a8d34d98c016438fe518cd530244c0338381244
QueenDomzy <qdomzy@gmail.com>
2025-09-15 13:46:33 +0100

    npm install @nestjs/typeorm typeorm pg

--- Backend/src/bookings/booking.entity.ts
+++ Backend/src/bookings/booking.entity.ts
@@ -4,20 +4,20 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Booking {
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  id!: string;
 
   @Column()
-  userId: string;
+  userId!: string;
 
   @Column()
-  roomId: string;
+  roomId!: string;
 
   @Column({ type: 'date' })
-  checkIn: string;
+  checkIn!: string;
 
   @Column({ type: 'date' })
-  checkOut: string;
+  checkOut!: string;
 
   @Column({ type: 'date', default: () => 'CURRENT_DATE' })
-  created: string;
-  }
+  created!: string;
+}
