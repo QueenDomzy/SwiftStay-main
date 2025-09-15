d3512694884e912bb4a80de02133147251f2a2d5
QueenDomzy <qdomzy@gmail.com>
2025-09-15 02:10:12 +0100

    npm install recharts npm install --save-dev @types/recharts

--- Frontend/src/@types/recharts.d.ts
+++ Frontend/src/@types/recharts.d.ts
@@ -1,1 +1,14 @@
-declare module "recharts";
+declare module "recharts" {
+  import * as React from "react";
+
+  export const LineChart: React.FC<any>;
+  export const Line: React.FC<any>;
+  export const XAxis: React.FC<any>;
+  export const YAxis: React.FC<any>;
+  export const Tooltip: React.FC<any>;
+  export const Legend: React.FC<any>;
+  export const ResponsiveContainer: React.FC<any>;
+  export const CartesianGrid: React.FC<any>;
+  export const BarChart: React.FC<any>;
+  export const Bar: React.FC<any>;
+}
