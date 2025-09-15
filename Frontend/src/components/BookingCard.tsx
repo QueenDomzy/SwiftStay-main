8bfd8a693b053ea6910c8a022156c17d7debc937
QueenDomzy <qdomzy@gmail.com>
2025-09-15 02:45:41 +0100

    npm run build

---
+++ Frontend/src/components/BookingCard.tsx
@@ -0,0 +1,11 @@
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+
+export function Card({ children, className, ...props }: CardProps) {
+  return (
+    <div className={`rounded-lg shadow-md ${className || ""}`} {...props}>
+      {children}
+    </div>
+  );
+      }
