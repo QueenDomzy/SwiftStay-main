c358bd2cc2809d784477ae71e3600c14d962febd
QueenDomzy <qdomzy@gmail.com>
2025-09-15 12:17:19 +0100

    npm install

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,8 +1,10 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { BookingsModule } from './bookings/bookings.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [BookingsModule],
+  imports: [BookingsModule],   // keep your bookings features
+  controllers: [AppController], // add AppController
+  providers: [],
 })
 export class AppModule {}
