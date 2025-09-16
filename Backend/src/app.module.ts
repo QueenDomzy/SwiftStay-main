98987d4368105016e6e50f015cfbb00be0369323
QueenDomzy <qdomzy@gmail.com>
2025-09-16 10:24:55 +0100

    npm install npm run build npm start

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,26 +1,11 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { HotelsModule } from './hotels/hotels.module';
 import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        url: config.get<string>('DATABASE_URL'), // Must set in Render env vars
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
-    HotelsModule,
-    BookingsModule,
-  ],
+  imports: [HotelsModule, BookingsModule],
   controllers: [AppController],
 })
 export class AppModule {}
