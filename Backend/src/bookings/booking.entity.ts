b93d918c218b5f0ccb5cdb3b447b2997a1cc898c
QueenDomzy <qdomzy@gmail.com>
2025-09-16 13:19:37 +0100

    npm run build

--- Backend/src/bookings/booking.entity.ts
+++ Backend/src/bookings/booking.entity.ts
@@ -1,11 +1,11 @@
 import { Hotel } from "../hotels/hotel.entity";
 
 export class Booking {
-  id: number;
-  guestName: string;
-  checkInDate: Date;
-  checkOutDate: Date;
-  hotelId: number;
+  id!: number;
+  guestName!: string;
+  checkInDate!: Date;
+  checkOutDate!: Date;
+  hotelId!: number;
 
   // Relation
   hotel?: Hotel;
