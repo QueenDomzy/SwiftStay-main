e0631fc821cc6239be14a55c38f26360a791ac55
QueenDomzy <qdomzy@gmail.com>
2025-09-16 12:25:23 +0100

    npm install npm run build npm start

--- Backend/src/hotels/hotel.entity.ts
+++ Backend/src/hotels/hotel.entity.ts
@@ -5,7 +5,7 @@ import { Booking } from '../bookings/booking.entity';
 export class Hotel {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
-  
+
   @Column()
   name!: string;
 
@@ -16,24 +16,4 @@ export class Hotel {
   bookings!: Booking[];
 }
 
-// Export explicitly
-export type { Hotel };import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-import { Hotel } from '../hotels/hotel.entity';
-
-@Entity()
-export class Booking {
-  @PrimaryGeneratedColumn('uuid')
-  id!: string;
-
-  @Column()
-  guestName!: string;
-
-  @Column({ type: 'date' })
-  checkInDate!: string;
-
-  @Column({ type: 'date' })
-  checkOutDate!: string;
-
-  @ManyToOne(() => Hotel, (hotel) => hotel.bookings)
-  hotel!: Hotel;
-}
+export { Hotel };
