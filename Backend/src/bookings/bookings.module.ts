0616631364899dba15d1a004f87105d9b5d59983
QueenDomzy <qdomzy@gmail.com>
2025-09-17 00:13:58 +0100

    npm install npm run build npm start

--- Backend/src/bookings/bookings.module.ts
+++ Backend/src/bookings/bookings.module.ts
@@ -2,10 +2,11 @@
 import { Module } from '@nestjs/common';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Module({
   controllers: [BookingsController],
-  providers: [BookingsService],
-  exports: [BookingsService],
+  providers: BookingsService, PrismaService. JwtAuthGuard],
 })
 export class BookingsModule {}
