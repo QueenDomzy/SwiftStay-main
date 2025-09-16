9871652a38637a2267d417686bbf7792d2e119d8
QueenDomzy <qdomzy@gmail.com>
2025-09-16 15:43:30 +0100

    Update create-booking.dto.tsnpm install npm run build npm start

--- Backend/src/bookings/dto/create-booking.dto.ts
+++ Backend/src/bookings/dto/create-booking.dto.ts
@@ -1,1 +1,8 @@
-
+// src/bookings/dto/create-booking.dto.ts
+export class CreateBookingDto {
+  guestName!: string;
+  checkIn!: string;  // ISO date string
+  checkOut!: string;
+  hotelId!: number;
+  roomId!: number;
+}
