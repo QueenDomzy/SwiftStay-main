beda751e1f313dcdefea5b6565d818d695b26afc
QueenDomzy <qdomzy@gmail.com>
2025-09-16 23:44:39 +0100

    Create user.module.ts

---
+++ Backend/src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService, PrismaService, JwtAuthGuard],
+  exports: [UserService],
+})
+export class UserModule {}
