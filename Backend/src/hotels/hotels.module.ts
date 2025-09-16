5034497927020a3cf684436964ee329605ff89c2
QueenDomzy <qdomzy@gmail.com>
2025-09-16 14:27:11 +0100

    npm install @prisma/client prisma npm install @nestjs/common @nestjs/core @nestjs/platform-express reflect-metadata rxjs # plus dev: typescript, ts-node, @types/node etc. npx prisma generate npx prisma migrate dev --name init

--- Backend/src/hotels/hotels.module.ts
+++ Backend/src/hotels/hotels.module.ts
@@ -1,14 +1,10 @@
 // src/hotels/hotels.module.ts
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Hotel } from './hotel.entity';
-import { HotelsService } from './hotels.service';
 import { HotelsController } from './hotels.controller';
+import { HotelsService } from './hotels.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Hotel])],
   controllers: [HotelsController],
   providers: [HotelsService],
-  exports: [TypeOrmModule], // Export repository to other modules
 })
 export class HotelsModule {}
