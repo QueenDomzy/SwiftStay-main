f0a75d7a0b8de2565815e553bbda4f0aaf3ffc47
QueenDomzy <qdomzy@gmail.com>
2025-09-17 09:51:40 +0100

    npm install @nestjs/passport passport passport-jwt @nestjs/jwt npm in…

--- Backend/src/auth/auth.module.ts
+++ Backend/src/auth/auth.module.ts
@@ -2,20 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your_jwt_secret', // Use env variable in production
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
-  providers: [AuthService, PrismaService, JwtStrategy],
+  imports: [JwtModule.register({ secret: process.env.JWT_SECRET || 'secretKey', signOptions: { expiresIn: '1h' } })],
   controllers: [AuthController],
+  providers: [AuthService, PrismaService],
   exports: [AuthService],
 })
 export class AuthModule {}
