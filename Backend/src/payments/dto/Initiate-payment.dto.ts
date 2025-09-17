6ed0ba587f894991018659c9c8384f3380b544d5
QueenDomzy <qdomzy@gmail.com>
2025-09-17 11:15:54 +0100

    npm install class-validator class-transformer

---
+++ Backend/src/payments/dto/Initiate-payment.dto.ts
@@ -0,0 +1,10 @@
+// src/payments/dto/initiate-payment.dto.ts
+import { IsInt, IsPositive } from 'class-validator';
+
+export class InitiatePaymentDto {
+  @IsInt()
+  userId: number;
+
+  @IsPositive()
+  amount: number;
+}
