f31682cce6780779fda3086109c33a14c1228de6
QueenDomzy <qdomzy@gmail.com>
2025-09-17 02:26:15 +0100

    cd Backend  # NestJS auth & JWT npm install @nestjs/jwt @nestjs/passport passport passport-jwt bcrypt  # Prisma client npm install @prisma/client  # Email & payments npm install nodemailer  # Type definitions for TS npm install --save-dev @types/bcrypt @types/passport-jwt @types/nodemailer

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,18 +1,6 @@
-// src/app.module.ts
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
-import { HotelsModule } from './hotels/hotels.module';
-import { BookingsModule } from './bookings/bookings.module';
-import { AppController } from './app.controller';
+import { HotelModule } from './hotels/hotels.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    HotelsModule,
-    BookingsModule,
-  ],
-  controllers: [AppController],
+  imports: [HotelModule, AuthModule, BookingModule, PaymentModule],
 })
 export class AppModule {}
