0c9c45ed13a4e3cf4f4c406f2362894b8fbdfcad
QueenDomzy <qdomzy@gmail.com>
2025-09-17 02:19:43 +0100

    cd Backend  # NestJS auth & JWT npm install @nestjs/jwt @nestjs/passport passport passport-jwt bcrypt  # Prisma client npm install @prisma/client  # Email & payments npm install nodemailer  # Type definitions for TS npm install --save-dev @types/bcrypt @types/passport-jwt @types/nodemailer

--- Backend/src/hotels/hotels.module.ts
+++ Backend/src/hotels/hotels.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { HotelService } from './hotel.service';
 import { HotelController } from './hotel.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Module({
   controllers: [HotelController],
-  providers: [HotelService, PrismaService, JwtAuthGuard],
+  providers: [HotelService, PrismaService],
 })
 export class HotelModule {}
