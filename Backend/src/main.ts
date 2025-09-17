d1df3550cebf23341a255c51316595c471997f35
QueenDomzy <qdomzy@gmail.com>
2025-09-17 11:40:11 +0100

    npm install class-validator class-transformer

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,10 +1,12 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   const port = parseInt(process.env.PORT || '4000', 10);
   await app.listen(port, '0.0.0.0');
   console.log(`🚀 SwiftStay (Prisma) running on port ${port}`);
