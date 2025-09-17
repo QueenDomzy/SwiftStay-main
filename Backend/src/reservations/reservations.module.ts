77a964e6146f917fe5b24016adaea109df9a4273
QueenDomzy <qdomzy@gmail.com>
2025-09-17 09:29:48 +0100

    npm install bcrypt passport passport-jwt @nestjs/jwt @nestjs/passport…

--- Backend/src/reservations/reservations.module.ts
+++ Backend/src/reservations/reservations.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ReservationsService } from './reservation.service';
+import { ReservationService } from './reservation.service';
 import { ReservationsController } from './reservations.controller';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [ReservationsController],
-  providers: [ReservationsService, PrismaService],
+  providers: [ReservationService, PrismaService],
+  exports: [ReservationService],
 })
 export class ReservationsModule {}
