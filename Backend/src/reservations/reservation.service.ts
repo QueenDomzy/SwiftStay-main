2acaa3de439ac9d79c62359b5e81ec8cbabf4304
QueenDomzy <qdomzy@gmail.com>
2025-09-17 10:49:59 +0100

    npm install @nestjs/jwt @nestjs/passport passport passport-jwt bcrypt nodemailer npm install --save-dev @types/bcrypt @types/passport-jwt @types/nodemailer

--- Backend/src/reservations/reservation.service.ts
+++ Backend/src/reservations/reservation.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 
 @Injectable()
-export class ReservationService {
+export class ReservationsService {
   constructor(private prisma: PrismaService) {}
 
   async createReservation(data: CreateReservationDto) {
