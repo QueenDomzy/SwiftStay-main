4205f6de3dcf97b7103184ba202e1589f3198823
QueenDomzy <qdomzy@gmail.com>
2025-09-17 03:54:21 +0100

    npm install bcrypt @nestjs/jwt @nestjs/passport passport passport-jwt nodemailer openai npm install --save-dev @types/bcrypt @types/passport-jwt @types/nodemailer  npm run build npm run start:dev

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookingModule } from './bookings/bookings.module';
-import { PaymentModule } from './payments/payments.module';
 import { HotelModule } from './hotels/hotels.module';
+import { AiModule } from './ai/ai.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ReservationsModule } from './reservations/reservations.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [AuthModule, BookingModule, PaymentModule, HotelModule],
+  imports: [
+    AuthModule,
+    BookingModule,
+    HotelModule,
+    ReservationsModule,
+    PaymentsModule,
+    AiModule,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
